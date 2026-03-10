396eaaa8e9f178858a3d44167ca235906de956b4
DARC000G <dawidpasinski.p@gmail.com>
2026-03-10 20:09:57 +0100

    Test 2

--- Universitet_System/obj/Debug/net10.0/Helloworld.AssemblyInfo.cs
+++ Universitet_System/obj/Debug/net10.0/Helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa7c9f9e39e29c2d0586b384bb53adda8cdc72d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b8e1d17f09489d472277e1e872db53bf13f1d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
